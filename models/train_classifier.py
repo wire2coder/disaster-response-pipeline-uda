1d9baa91200b2958f331eb0dc50a9bb6923f2761
Terry S <wire2coder@users.noreply.github.com>
2023-04-07 09:27:17 +0000

    0527_FriApril2023

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -10,20 +10,25 @@ Arguments:
     2) Path to pickle file name where ML model needs to be saved (e.g. classifier.pkl)
 """
 
+# import libraries
+import nltk
+nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download('averaged_perceptron_tagger')
+
+# import libraries
+import numpy as np
+import pandas as pd
+pd.set_option('display.max_columns', 500)
+
 import sys
 import os
 import re
 from sqlalchemy import create_engine
 import pickle
 
-# import libraries
-import numpy as np
-import pandas as pd
-
-import nltk
-nltk.download(['punkt', 'wordnet', 'averaged_perceptron_tagger'])
-
 from scipy.stats import gmean
+# import relevant functions/modules from the sklearn
 from sklearn.pipeline import Pipeline, FeatureUnion
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
@@ -34,6 +39,7 @@ from sklearn.model_selection import GridSearchCV
 from sklearn.feature_extraction.text import TfidfTransformer, CountVectorizer
 from sklearn.multioutput import MultiOutputClassifier
 from sklearn.base import BaseEstimator,TransformerMixin
+
 
 
 
@@ -68,8 +74,35 @@ def load_data(database_filepath):
     category_names = y.columns
     return X, y, category_names
 
-def tokenize(text):
-    pass
+def tokenize(text, url_place_holder_string="urlplaceholder"):
+    """
+    Tokenize the text function
+    
+    Arguments:
+        text -> Text message which needs to be tokenized
+    Output:
+        clean_tokens -> List of tokens extracted from the provided text
+    """
+    
+    # Replace all urls with a urlplaceholder string
+    url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+    
+    # Extract all the urls from the provided text 
+    detected_urls = re.findall(url_regex, text)
+    
+    # Replace url with a url placeholder string
+    for detected_url in detected_urls:
+        text = text.replace(detected_url, url_place_holder_string)
+
+    # Extract the word tokens from the provided text
+    tokens = nltk.word_tokenize(text)
+    
+    #Lemmanitizer to remove inflectional and derivationally related forms of a word
+    lemmatizer = nltk.WordNetLemmatizer()
+
+    # List of clean tokens
+    clean_tokens = [lemmatizer.lemmatize(w).lower().strip() for w in tokens]
+    return clean_tokens
 
 
 # Build a custom transformer which will extract the starting verb of a sentence
@@ -103,10 +136,94 @@ class StartingVerbExtractor(BaseEstimator, TransformerMixin):
 
 
 def build_pipeline():
-    pass
+    pipeline = Pipeline([
+        ('features', FeatureUnion([
 
-def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+            ('text_pipeline', Pipeline([
+                ('count_vectorizer', CountVectorizer(tokenizer=tokenize)),
+                ('tfidf_transformer', TfidfTransformer())
+            ])),
+
+            ('starting_verb_transformer', StartingVerbExtractor())
+        ])),
+
+        ('classifier', MultiOutputClassifier(AdaBoostClassifier()))
+    ])
+
+    return pipeline
+
+
+def multioutput_fscore(y_true,y_pred,beta=1):
+    """
+    MultiOutput Fscore
+
+    It is a sort of geometric mean of the fbeta_score, computed on each label.
+    
+    It is compatible with multi-label and multi-class problems.
+    It features some peculiarities (geometric mean, 100% removal...) to exclude
+    trivial solutions and deliberatly under-estimate a stangd fbeta_score average.
+    The aim is avoiding issues when dealing with multi-class/multi-label imbalanced cases.
+    
+    It can be used as scorer for GridSearchCV:
+        scorer = make_scorer(multioutput_fscore,beta=1)
+        
+    Arguments:
+        y_true -> List of labels
+        y_prod -> List of predictions
+        beta -> Beta value to be used to calculate fscore metric
+    
+    Output:
+        f1score -> Calculation geometric mean of fscore
+    """
+    
+    # If provided y predictions is a dataframe then extract the values from that
+    if isinstance(y_pred, pd.DataFrame) == True:
+        y_pred = y_pred.values
+    
+    # If provided y actuals is a dataframe then extract the values from that
+    if isinstance(y_true, pd.DataFrame) == True:
+        y_true = y_true.values
+    
+    f1score_list = []
+    for column in range(0,y_true.shape[1]):
+        score = fbeta_score(y_true[:,column],y_pred[:,column],beta,average='weighted')
+        f1score_list.append(score)
+        
+    f1score = np.asarray(f1score_list)
+    f1score = f1score[f1score<1]
+    
+    # Get the geometric mean of f1score
+    f1score = gmean(f1score)
+
+    return f1score
+
+
+def evaluate_pipeline(pipeline, X_test, Y_test, category_names):
+    """
+    Evaluate Model function
+    
+    This function applies a ML pipeline to a test set and prints out the model performance (accuracy and f1score)
+    
+    Arguments:
+        pipeline -> A valid scikit ML Pipeline
+        X_test -> Test features
+        Y_test -> Test labels
+        category_names -> label names (multi-output)
+    """
+    Y_pred = pipeline.predict(X_test)
+    
+    # multi_f1 = multioutput_fscore(Y_test,Y_pred, beta = 1)
+    overall_accuracy = (Y_pred == Y_test).mean().mean()
+
+    print('Average overall accuracy {0:.2f}%'.format(overall_accuracy*100))
+    # print('F1 score (custom definition) {0:.2f}%'.format(multi_f1*100))
+
+    # Print the whole classification report.
+    Y_pred = pd.DataFrame(Y_pred, columns = Y_test.columns)
+    
+    for column in Y_test.columns:
+        print('Model Performance with Category: {}'.format(column))
+        print(classification_report(Y_test[column],Y_pred[column]))
 
 
 def save_model(model, model_filepath):
@@ -131,8 +248,8 @@ def main():
         print('Training model...')
         model.fit(X_train, Y_train)
         
-        # print('Evaluating model...')
-        # evaluate_model(model, X_test, Y_test, category_names)
+        print('Evaluating model...')
+        evaluate_pipeline(model, X_test, Y_test, category_names)
 
         # print('Saving model...\n    MODEL: {}'.format(model_filepath))
         # save_model(model, model_filepath)
